16a02f7d7cf48380bfded6519d47854b2cb4144e
Sergio Ortega <instintoimagen@gmail.com>
2024-07-17 14:12:35 -0300

    Td001 - Tailwind ok

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "main-color": "#FAFFAF",
+        "sec-color": "#96C9F4",
+        "thi-color": "#3FA2F6",
+        "for-color": "#0F67B1",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
+  },
+  plugins: [],
+};
